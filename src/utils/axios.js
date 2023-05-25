b3f17bf31f39ea5dbea015ed19c471e74358d9ef
technolitics <backup.technolitics@gmail.com>
2023-05-25 15:42:44 +0530

    pushed

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,6 +1,8 @@
 
 import axios from 'axios';
-const BASE_URL = 'http://localhost:8000/supperadmin';
+// const BASE_URL = 'http://localhost:8000/supperadmin';
+const BASE_URL = 'https://genricapi.mytechnoschool.com/supperadmin';
+
 const axiosInstance = axios.create({
   baseURL: BASE_URL,
 });
