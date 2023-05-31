9074f3ca047bd96ea291218d23f33b397da38e6e
technolitics <backup.technolitics@gmail.com>
2023-05-31 10:20:38 +0530

    pushed

--- src/App.js
+++ src/App.js
@@ -1,40 +1,40 @@
 // i18n
 import './locales/i18n';
 
-// scroll bar
-import 'simplebar/src/simplebar.css';
+// scroll bar a
+import 'simplebar/dist/simplebar.css';
 
 // lightbox
 /* eslint-disable import/no-unresolved */
-import 'yet-another-react-lightbox/styles.css';
 import 'yet-another-react-lightbox/plugins/captions.css';
 import 'yet-another-react-lightbox/plugins/thumbnails.css';
+import 'yet-another-react-lightbox/styles.css';
 
 // map
-import './utils/mapboxgl';
 import 'mapbox-gl/dist/mapbox-gl.css';
+import './utils/mapboxgl';
 
 // editor
 import 'react-quill/dist/quill.snow.css';
 
 // slick-carousel
-import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
+import 'slick-carousel/slick/slick.css';
 
 // lazy image
 import 'react-lazy-load-image-component/src/effects/blur.css';
 
 // ----------------------------------------------------------------------
-
-import { BrowserRouter } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { HelmetProvider } from 'react-helmet-async';
 import { Provider as ReduxProvider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 import { PersistGate } from 'redux-persist/lib/integration/react';
 // @mui
-import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
 import { LocalizationProvider } from '@mui/x-date-pickers';
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
 // redux
-import { store, persistor } from './redux/store';
+import { persistor, store } from './redux/store';
 // routes
 import Router from './routes';
 // theme
@@ -42,11 +42,10 @@ import ThemeProvider from './theme';
 // locales
 import ThemeLocalization from './locales';
 // components
-import { StyledChart } from './components/chart';
-import SnackbarProvider from './components/snackbar';
-import ScrollToTop from './components/scroll-to-top';
 import { MotionLazyContainer } from './components/animate';
-import { ThemeSettings, SettingsProvider } from './components/settings';
+import ScrollToTop from './components/scroll-to-top';
+import { SettingsProvider, ThemeSettings } from './components/settings';
+import SnackbarProvider from './components/snackbar';
 
 // Check our docs
 // https://docs.minimals.cc/authentication/js-version
@@ -57,35 +56,38 @@ import { AuthProvider } from './auth/JwtContext';
 // import { AuthProvider } from './auth/AwsCognitoContext';
 
 // ----------------------------------------------------------------------
+// Create a client
+const queryClient = new QueryClient();
 
 export default function App() {
   return (
-    <AuthProvider>
-      <HelmetProvider>
-        <ReduxProvider store={store}>
-          <PersistGate loading={null} persistor={persistor}>
-            <LocalizationProvider dateAdapter={AdapterDateFns}>
-              <SettingsProvider>
-                <BrowserRouter>
-                  <ScrollToTop />
-                  <MotionLazyContainer>
-                    <ThemeProvider>
-                      <ThemeSettings>
-                        <ThemeLocalization>
-                          <SnackbarProvider>
-                            <StyledChart />
-                            <Router />
-                          </SnackbarProvider>
-                        </ThemeLocalization>
-                      </ThemeSettings>
-                    </ThemeProvider>
-                  </MotionLazyContainer>
-                </BrowserRouter>
-              </SettingsProvider>
-            </LocalizationProvider>
-          </PersistGate>
-        </ReduxProvider>
-      </HelmetProvider>
-    </AuthProvider>
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <HelmetProvider>
+          <ReduxProvider store={store}>
+            <PersistGate loading={null} persistor={persistor}>
+              <LocalizationProvider dateAdapter={AdapterDateFns}>
+                <SettingsProvider>
+                  <BrowserRouter>
+                    <ScrollToTop />
+                    <MotionLazyContainer>
+                      <ThemeProvider>
+                        <ThemeSettings>
+                          <ThemeLocalization>
+                            <SnackbarProvider>
+                              <Router />
+                            </SnackbarProvider>
+                          </ThemeLocalization>
+                        </ThemeSettings>
+                      </ThemeProvider>
+                    </MotionLazyContainer>
+                  </BrowserRouter>
+                </SettingsProvider>
+              </LocalizationProvider>
+            </PersistGate>
+          </ReduxProvider>
+        </HelmetProvider>
+      </AuthProvider>
+    </QueryClientProvider>
   );
 }
